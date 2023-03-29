3be384aa2f689a9f996c6e8e6df401ad6199342f
deanmaher <72203596+deanmaher@users.noreply.github.com>
2023-03-29 10:36:08 +0100

    Add files via upload

---
+++ dean_storyboard_previsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dean_storyboard_previsApp.swift
+//  dean_storyboard_previs
+//
+//  Created by Student on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct dean_storyboard_previsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
